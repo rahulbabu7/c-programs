4aa8e180b3828591c2b533f374b108b9e3b85fe6
rahulbabu7 <rahulbabu436@gmail.com>
2023-08-05 16:32:04 +0530

    updated

--- s6 lab/distancevector.c
+++ s6 lab/distancevector.c
@@ -19,18 +19,20 @@ rt[i].from[j]=j; // initialize the source node
 }
 }
 do {
-count=0;
-for(i=0;i<nodes;i++) 
-//and add the distance from k to node j
-for(j=0;j<nodes;j++)
-for(k=0;k<nodes;k++)
-if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j]) {
-//We calculate the minimum distance
-rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-rt[i].from[j]=k;
-count++;
-}
-} while(count!=0);
+    count = 0;
+    for (i = 0; i < nodes; i++) {
+        for (j = 0; j < nodes; j++) {
+            for (k = 0; k < nodes; k++) {
+                if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) {
+                    rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
+                    rt[i].from[j] = k;
+                    count++;
+                }
+            }
+        }
+    }
+} while (count != 0);
+
 for(i=0;i<nodes;i++) {
 printf("\n\nFor router %d\n",i+1);
 for(j=0;j<nodes;j++) {
